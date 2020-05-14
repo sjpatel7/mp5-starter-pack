2e302e012532ed49a0b91fbce9197d965f1174b4
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-05-14 03:40:30 -0500

    Update part_c.py

--- part_c.py
+++ part_c.py
@@ -11,7 +11,15 @@ def get_shortest_distances(graphframe, dst_id):
     # Find shortest distances in the given graphframe to the vertex which has id `dst_id`
     # The result is a dictionary where key is a vertex id and the corresponding value is
     # the distance of this node to vertex `dst_id`.
-    return {}
+    result = {}
+    paths = graphframe.shortestPaths(landmarks=[dst_id])
+    df = paths.select("id", "distances").collect()
+    for row in df:
+        src = row.id
+        d = row.distances(dst_id)
+        result[src] = d                            
+    return result
+    #return {}
 
 
 if __name__ == "__main__":
@@ -19,15 +27,16 @@ if __name__ == "__main__":
     edge_list = []
     with open('dataset/graph.data') as f:
         for line in f:
+            verticies = line.rstrip().split(' ')
             # TODO: Parse line to get vertex id
-            src = None
+            src = verticies[0]
             # TODO: Parse line to get ids of vertices that src is connected to
-            dst_list = []
+            dst_list = verticies[1:]
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create dataframe for vertices
-    edges = spark.createDataFrame(...)  # TODO: Create dataframe for edges
+    vertices = spark.createDataFrame(vertex_list, ["id"])  # TODO: Create dataframe for vertices
+    edges = spark.createDataFrame(edge_list, ["src", "dst"])  # TODO: Create dataframe for edges
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/shortest-paths")
