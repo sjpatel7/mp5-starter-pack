1ab10e0692f6ad683d450168555c065871f0d104
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-05-13 22:23:27 -0500

    worked in jupyter

--- part_b_mllib.py
+++ part_b_mllib.py
@@ -25,15 +25,35 @@ def get_clusters(data_rdd, num_clusters=NUM_CLUSTERS, max_iterations=MAX_ITERATI
     # For example, if the output is [["Mercedes", "Audi"], ["Honda", "Hyundai"]]
     # Then "Mercedes" and "Audi" should have the same cluster id, and "Honda" and
     # "Hyundai" should have the same cluster id
-    return [[]]
+    cars = {}
+    for line in data_rdd.collect():
+        point = line.split(',')
+        cars[point[0]] = array([x for x in point[1:]])
+    
+    points = data_rdd.map(lambda line: array([float(x) for x in line.split(',')[1:]]))
+    clusters = KMeans.train(points, 4, maxIterations=100, initializationMode="random")
+    car_clusters = {}
+    ct = 0
+    for car in cars:
+        features = cars[car]
+        c = clusters.predict(features)
+        car_clusters.setdefault(c, []).append(car)
+        
+    result = [[]]
+    for c in car_clusters:
+        cluster = []
+        for car in car_clusters[c]:
+            cluster.append(car)
+        result.append(cluster)
+    result.remove([])
+    return result
 
 
 if __name__ == "__main__":
     f = sc.textFile("dataset/cars.data")
 
     # TODO: Parse data from file into an RDD
-    data_rdd = None
-    clusters = get_clusters(data_rdd)
+    clusters = get_clusters(f)
 
     for cluster in clusters:
         print(','.join(cluster))
